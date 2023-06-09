e923056869d23a09c678984d312a209b212a4cb8
fearofgod0001 <dlxo8888@gmail.com>
2023-06-09 12:33:14 +0900

    JPA entity 단위Test CART 조회

--- src/main/java/com/kh/finalPrjAm/entity/Order.java
+++ src/main/java/com/kh/finalPrjAm/entity/Order.java
@@ -7,6 +7,8 @@ import lombok.ToString;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "orders")
@@ -20,12 +22,16 @@ public class Order {
     @JoinColumn(name = "member_id") //회원정보
     private Member member;
 
+    //주문 상품 엔티티와 일대다 매핑
+    // 외래키(order_id)가 order_item테이블에 있으므로 연관 관계의 주인은 OrderItem엔티티입니다.
+    // Order 엔티티가 주인이 아니므로 "mappedBy" 속성으로 연관 관계의 주인을 설정합니다.
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItemList = new ArrayList<>();
+
+    //주문 상품 엔티티와 일대다 매핑
     private LocalDateTime orderDate; // 주문일
     @Enumerated(EnumType.STRING)
     private OrderStatus orderStatus;
     private LocalDateTime regTime;
     private LocalDateTime updateTime;
-
-
-
 }
