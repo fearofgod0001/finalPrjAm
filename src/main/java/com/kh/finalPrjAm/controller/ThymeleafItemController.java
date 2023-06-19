5812931fb26e4cc08f1737e91ed0c8d7882d164b
fearofgod0001 <dlxo8888@gmail.com>
2023-06-19 10:03:47 +0900

    Thymeleaf 추가 꼭 볼 것

---
+++ src/main/java/com/kh/finalPrjAm/controller/ThymeleafItemController.java
@@ -0,0 +1,34 @@
+package com.kh.finalPrjAm.controller;
+
+import com.kh.finalPrjAm.dto.ItemDto;
+import com.kh.finalPrjAm.entity.Item;
+import com.kh.finalPrjAm.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Slf4j //logging data 출력 혹은 저장
+// log.error 등 가독성이 좋아짐 컬러를 설정 가능
+// logging data를 따로 저장하여 문제가 발생한 시점을 파악 할 수 있다.
+@Controller //view로 바로 던질 때 사용
+@RequiredArgsConstructor//JSON이나  XML로 던질 때 사용
+@RequestMapping("/thymeleaf")
+public class ThymeleafItemController {
+    private final ItemService itemService;
+    @GetMapping("/items")
+    public String getItemList(Model model){
+        List<ItemDto> itemDtoList = itemService.getItemList();
+        model.addAttribute("itemDtoList",itemDtoList);
+        return "thymeleaf/thymeleafItemList";
+    }
+    @GetMapping("/new")
+    public String showItemForm(Model model){
+        model.addAttribute("item",new Item());
+        return "thymeleaf/item-form";
+    }
+}
